d6fbc26156f494dab42bce3641e2a066dd9b1b6d
keerthanahestia <keerthana@hestiatechnology.com>
2026-02-16 11:38:39 +0530

    route

--- src/DSO_ADMIN_CONNECT/Routes/Route.tsx
+++ src/DSO_ADMIN_CONNECT/Routes/Route.tsx
@@ -18,7 +18,42 @@ export const dsoadminConnectRoutes = (
     // }
   >
 <Route index element={<HomePage/>} />
-    <Route path="analytics" element={<div><h5>Analytics</h5></div>} />
+<Route path="analytics" element={<div><h5>Analytics</h5></div>} />
+    {/* <Route path="/dsoadmin-connect/home/caseOnHold" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/home/inTransit" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/home/inProduction" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/home/submitted" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/home/recent" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/analytics" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/masters/lab" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/masters/practice" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/masters/doctor" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/masters/productGroup" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/masters/userRoleCreation" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/setup/practiceManagerLogin" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/setup/dsoUserLogin" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/setup/labProductsRate" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/setup/zone" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/mapping/prescriptionProduct" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/mapping/triosDoctor" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/mapping/triosProduct" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/mapping/labDsoDoctor" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/mapping/labDsoProduct" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/mapping/doctorPickupService" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/reports/caseSummary" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/reports/revenueSummary" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/reports/supportTickets" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/reports/caseOnHoldAging" element={</>} /> */}
+    {/* <Route path="/dsoadmin-connect/reports/usageAdoptionReport" element={</>} /> */}
+
+    {/* <Route path="/dsoadmin-connect/invoices/proformaInvoice" element={</>} /> */}
+    
+    
     <Route path="masters" element={<div><h5>User Management</h5></div>} />
      <Route path="setup" element={<div><h5>User Management</h5></div>} />
       <Route path="mapping" element={<div><h5>User Management</h5></div>} />
